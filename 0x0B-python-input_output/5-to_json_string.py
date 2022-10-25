6e59bdb9bc481461ed54bb228aa6e69c51632794
peaceissa <issare78@gmail.com>
2022-10-25 12:56:24 -0700

    task upload

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 5-to_json_string.
+Returns the JSON representation of an object.
+"""
+import json
+def to_json_string(my_obj):
+    """Returns the JSON representation of my obj.
+
+    Args:
+        - my_obj: string to represent
+
+    Returns: JSON representation
+    """"
+    return json.dumps(my_obj)
